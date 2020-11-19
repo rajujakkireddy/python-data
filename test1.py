5eb9d78e44bc4d1653b4fe4a1c73355ea63b5b64
Vicky <vicky@gmail.com>
2020-11-19 13:18:15 +0530

    adding test file

---
+++ test1.py
@@ -0,0 +1,49 @@
+'''if True:
+	print("if")
+	print("something")
+else:
+	print("else")
+'''
+'''
+name = 'The Sublime Text is configuring python instead of python3.\
+You just need to add a new build system to the Sublime Text. \
+To do that, Select Tools > Build System > New Build System... \
+Now, a new file will open. Add the following code to this file:'
+print(name)
+'''
+'''
+name = ['The Sublime Text is configuring python instead of python3. ',
+'You just need to add a new build system to the Sublime Text. To do that, Select Tools > Build System > New Build System... Now, a new file will open. Add the following code to this file:']
+print(name)
+'''
+"""
+statement = '''I'm "Prashanth"'''
+print(statement)
+"""
+"""
+name = '''The Sublime Text is configuring python instead of python3.\
+You just need to add a new build system to the Sublime Text. \
+To do that, Select Tools > Build System > New Build System... \
+Now, a new file will open. Add the following code to this file:'''
+print(name)
+"""
+# single line commment
+
+''' multi line comment '''
+'''print("hello");print("world")'''
+'''a=2;b=3;print(a,b)'''
+'''a = b = c = 10
+a, b, c = 10, 20, 30
+print(a,b,c)'''
+'''if True:
+	print("if")
+	print("something")
+else:
+	print("else")'''
+# Correct:
+# easy to match operators with operands
+income = (gross_wages
+          + taxable_interest
+          + (dividends - qualified_dividends)
+          - ira_deduction
+          - student_loan_interest)
